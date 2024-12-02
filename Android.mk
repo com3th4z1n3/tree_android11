331f917530e99d81f19942f9252c7ed7349e9479
com3th4z1n3 <exsenxxx@proton.me>
2024-12-02 08:32:30 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -4,4 +4,16 @@ LOCAL_PATH := device/realme/RMX3261
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcrypto
+LOCAL_SRC_FILES := /system/lib64/libcrypto.so  # Ganti dengan file sumber yang sesuai
+LOCAL_SHARED_LIBRARIES := libssl
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libkeymaster
+LOCAL_SRC_FILES := /system/lib64/libkeymaster_portable.so  # Ganti dengan file sumber yang sesuai
+LOCAL_SHARED_LIBRARIES := libcrypto
+include $(BUILD_SHARED_LIBRARY)
+
 endif
